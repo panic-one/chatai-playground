daa50e5a2583f0c38e326e20cde9d780d1c030a3
panic <0mt40k1z38c0d9w@gmail.com>
2026-03-04 21:40:21 +0900

    messagemodel更新

--- app/models/message.py
+++ app/models/message.py
@@ -6,7 +6,9 @@ class Message(db.Model):
 
     message_id = db.Column(db.Integer, primary_key=True)
     thread_id = db.Column(db.Integer, db.ForeignKey("threads.thread_id"), nullable=False)
-    firebase_uid = db.Column(db.String(128), nullable=False, index=True)
+    role = db.Column(db.String(20), nullable=False)
+    firebase_uid = db.Column(db.String(128), index=True)
+    model = db.Column(db.String(50))
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    message_index = db.Column(db.Integer)
+    message_index = db.Column(db.Integer, nullable=False)
