7c5c170652cefca0684508529c92cc2ef5c2ba89
panic <0mt40k1z38c0d9w@gmail.com>
2026-03-13 11:42:41 +0900

    エラー修正

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ app = create_app()
 if __name__ == "__main__":
     env = os.environ.get("ENV", "development")
     debug_mode = env == "development"
-    app.run(host="0.0.0.0", port=5000, debug=debug_mode)
+    app.run(host="0.0.0.0", port=5000, debug=debug_mode, use_reloader=False)
